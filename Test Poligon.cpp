3f544289db4ebf65e1ac53f928de16a4308c37e6
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2023-02-11 17:41:01 +0300

    Add files via upload

---
+++ Test Poligon.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+int main()
+{
+	for (int i = 0; i < 100; i++) {
+		setlocale(LC_ALL, "Ru");
+		std::cout << "Вычесление фарма за 1 час" << std::endl;
+		float Capital;
+		std::cout << "Введите количество хаосов затраченное на старт : ";
+		std::cin >> Capital;
+		std::cout << std::endl;
+		float time = 60;
+		float timez;
+		std::cout << "Введите количество минут затраченное на фарм : ";
+		std::cin >> timez;
+		std::cout << std::endl;
+		float timetotal = time / timez;
+		float farmZ;
+		std::cout << "Введите сколько хаосов получено за фарм : ";
+		std::cin >> farmZ;
+		std::cout << std::endl;
+		float totalFarm1Hour = farmZ * timetotal - Capital;
+		float totalFarm1HourDivine = ((farmZ - Capital) * timetotal)  / 260;
+		std::cout << "Грязными нафармлено за " << timez << " минут : " << farmZ << std::endl;
+		std::cout << "Чистый фарм за 1 час составляет (в хаосах) : " << totalFarm1Hour << std::endl;
+		std::cout << "Чистый фарм за 1 час составляет (в дивайнах) : " << totalFarm1HourDivine << std::endl;
+		std::cout << "------------------------------------------" << std::endl;
+	}
+}
